b53f700a45fedecfcb9d8e991a3cbd7fd265d0eb
An-GG <ankush.girotra@gmail.com>
2022-01-12 02:56:54 -0600

    line ordered

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,14 @@ const REPL_ON = true;
 var g = {} as any;
 
 let ses = new RegistrationSession(2022, 'Spring', new Map([
-    ['_shibsession_64656661756c7468747470733a2f2f75746469726563742e7574657861732e6564752f73686962626f6c657468','_f7821ef1ae6e49d35e35d3f830cb7010'],
-    ['ut_persist', '1896261824.47873.0000']
+    [
+        '_shibsession_64656661756c7468747470733a2f2f75746469726563742e7574657861732e6564752f73686962626f6c657468',
+        '_6c816f50630e31241613ae39d6a5865a'
+    ],
+    [
+        'ut_persist', 
+        '1896261824.47873.0000'
+    ]
 ]));
 let ack = await ses._fetch(ses.ut_direct_url+'registration/confirmEmailAddress.WBX', {});
 
